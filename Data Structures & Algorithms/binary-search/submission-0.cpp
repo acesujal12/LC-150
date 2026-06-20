f4dec7b455baf83ebb0a0f488d20c209a016394c
SUJAL NEELESH PATIL <sujalpatil546@gmail.com>
2026-06-20 23:16:04 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int binarySearch(vector<int>& nums, int low, int high, int target) {
+        if (low > high)
+    return -1;
+
+        int mid = (high + low) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+        else {
+            return binarySearch(nums, low, mid - 1, target);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+};
